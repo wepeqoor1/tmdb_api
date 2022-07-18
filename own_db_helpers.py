03cabdc98c957f9806a74124a4410a0c564ab519
Sourcery AI <>
2022-07-18 18:39:26 +0000

    'Refactored by Sourcery'

--- own_db_helpers.py
+++ own_db_helpers.py
@@ -5,5 +5,4 @@ def load_data(path):
     if not os.path.exists(path):
         return None
     with open(path, mode='r', encoding='utf-8') as my_file:
-        films_data = json.load(my_file)
-        return films_data
+        return json.load(my_file)
--- search_in_db.py
+++ search_in_db.py
@@ -1,11 +1,11 @@
 from own_db_helpers import load_data
 
 def search_for_film(keyword, films_data):
-    films_found = set()
-    for film in films_data:
-        if keyword.lower() in film['original_title'].lower():
-            films_found.add(film['original_title'])
-    return films_found
+    return {
+        film['original_title']
+        for film in films_data
+        if keyword.lower() in film['original_title'].lower()
+    }
 
 if __name__ == '__main__':
     path = input('Enter path to DataBase:')
